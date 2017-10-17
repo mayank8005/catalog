1a665247b750af623d73e2e288d663180e77ec85
vagrant <vagrant@vagrant.vm>
2017-10-17 17:54:51 +0000

    added db struc in datbase_setup file

---
+++ database_setup.py
@@ -0,0 +1,37 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+# declaration of user items table
+class Items(Base):
+    # setting name of the table
+    __tablename__ = 'items'
+
+    # declaration of table attributes
+    id = Column(Integer, primary_key=True)   # used as primary key
+    category = Column(String(100), nullable=False)  # predefined category
+    item_name = Column(String(256), nullable=False)     # stores name of item
+    item_description = Column(String(500))     # stores description of item
+    user_id = Column(Integer, ForeignKey('users.id'))  # stores uploader's id
+    users = relationship(Users)
+
+
+class Users(Base):
+
+    # setting name of the table
+    __tablename__ = 'users'
+
+    # declaration of table attributes
+    id = Column(Integer, primary_key=True)  # used as primary key
+    name = Column(String(100), nullable=False)   # name of the user
+    email = Column(String(256))     # email of user
+    picture = Column(String(256))   # URL of user's profile picture
+
+# creating engine for sqlalchemy
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
